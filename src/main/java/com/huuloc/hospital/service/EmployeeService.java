c2913940c23c5614041015d91f0d90e958a5f3da
lochuung <locn562836@gmail.com>
2023-11-30 22:59:15 +0700

    update jpa

--- src/main/java/com/huuloc/hospital/service/EmployeeService.java
+++ src/main/java/com/huuloc/hospital/service/EmployeeService.java
@@ -1,7 +1,9 @@
 package com.huuloc.hospital.service;
 
 import com.huuloc.hospital.entity.Employee;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface EmployeeService {
+public interface EmployeeService extends UserDetailsService {
     Employee getEmployeeById(long id);
+    Employee findByUsername(String username);
 }
--- src/main/java/com/huuloc/hospital/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/huuloc/hospital/service/impl/EmployeeServiceImpl.java
@@ -4,7 +4,15 @@ import com.huuloc.hospital.entity.Employee;
 import com.huuloc.hospital.repository.EmployeeRepository;
 import com.huuloc.hospital.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -20,5 +28,27 @@ public class EmployeeServiceImpl implements EmployeeService {
         return employeeRepository.findById(id).orElseThrow(
                 () -> new RuntimeException("Employee not found with id " + id)
         );
+    }
+
+    @Override
+    public Employee findByUsername(String username) {
+        return employeeRepository.findByUsername(username).orElseThrow(
+                () -> new RuntimeException("Employee not found with username " + username)
+        );
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Employee employee = employeeRepository.findByUsername(username).orElseThrow(
+                () -> new UsernameNotFoundException("Employee not found with username "
+                        + username)
+        );
+        return new User(employee.getUsername(), employee.getPassword(),
+                getAuthorities(employee));
+    }
+
+    private static Collection<? extends GrantedAuthority> getAuthorities(Employee employee) {
+        String role = "ROLE_" +  employee.getType().toUpperCase();
+        return AuthorityUtils.createAuthorityList(role);
     }
 }
